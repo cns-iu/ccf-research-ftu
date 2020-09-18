0b33a2c8cebe4c637f98acd8e03a1ce7c8b37732
Yingnan Ju <bunny_gg@hotmail.com>
2020-09-18 05:29:03 -0400

    added VU gold standard viz

--- visualization/viz_test.py
+++ visualization/viz_test.py
@@ -1,10 +1,10 @@
 import json
-import pandas as pd
-from collections import OrderedDict
+# from collections import OrderedDict
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import HoverTool
-from pandas.io.json import json_normalize
+# from bokeh.models import HoverTool
+import numpy as np
+from PIL import Image
 import os
 
 if __name__ == '__main__':
@@ -19,23 +19,23 @@ if __name__ == '__main__':
 
     folder = './annotations'
     file_names = os.listdir(folder)
-    size_list = [
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 1.02],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-        [2780, 3805, 0.9],
-    ]
+    size_index_list = [1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02,
+                       1.02, ]
+    rescale_index = 8
 
-    for file_name, size in zip(file_names, size_list):
+    for file_name, size_index in zip(file_names[:], size_index_list[:]):
         # file_name = './annotations/VAN0006-LK-2-85-AF_preIMS_registered_glomerulus_detections.json'
         output_name = file_name.split('_')[0]
+        image_name = f'result/images/{output_name}_preIMS_registered_8.jpg'
         output_file(f"result/{output_name}.html")
         with open(os.path.join(folder, file_name)) as data_file:
             data = json.load(data_file)
@@ -53,22 +53,30 @@ if __name__ == '__main__':
 
         # print(file_name, len(coor_list))
 
-        x_list = [[xy[0] for xy in coor] for coor in coor_list]
-        y_list = [[xy[1] for xy in coor] for coor in coor_list]
+        x_list = [[xy[0] // rescale_index for xy in coor] for coor in coor_list]
+        y_list = [[xy[1] // rescale_index for xy in coor] for coor in coor_list]
+
+        background_img = Image.open(image_name).convert('RGBA')
+        xdim, ydim = background_img.size
+        a_layer = np.empty((ydim, xdim), dtype=np.uint32)
+        view = a_layer.view(dtype=np.uint8).reshape((ydim, xdim, 4))
+        # view[:, :, :] = np.flipud(np.asarray(lena_img))
+        view[:, :, :] = np.asarray(background_img)
 
         p = figure(match_aspect=True,
-                   plot_width=int(size[0] * size[2]), plot_height=int(size[1] * size[2]),
+                   plot_width=int(xdim * size_index), plot_height=int(ydim * size_index),
                    tools=tools_list,
                    # title='nuclei/vessel distance',
                    )
 
-        p.image_url(url=[r'C:\Users\bunny\Desktop\VAN0006-LK-2-85-AF_preIMS_registered.ome.tiff - default.jpg'],
-                    x=0, y=0, anchor="bottom_left")
+        # p.image_url(url=[image_name], x=0, y=0, anchor="bottom_left")
+
+        p.image_rgba(image=[a_layer], x=0, y=0, dw=xdim, dh=ydim)
         p.xgrid.visible = False
         p.ygrid.visible = False
         p.axis.visible = False
         p.background_fill_alpha = 0.0
         p.outline_line_color = None
 
-        p.patches(x_list, y_list, fill_alpha=0, color='white', line_width=2)
+        p.patches(x_list, y_list, fill_alpha=0, line_alpha=0.5, color='pink', line_width=3)
         show(p)

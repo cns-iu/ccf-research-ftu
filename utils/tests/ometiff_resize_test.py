3f0ed6ef2c9cf9526434bec3b812d0288ac6a2c4
Yingnan Ju <bunny_gg@hotmail.com>
2020-10-13 15:29:29 -0400

    adapted for cell/nuclei mask separation

--- utils/tests/ometiff_resize_test.py
+++ utils/tests/ometiff_resize_test.py
@@ -5,7 +5,7 @@ os.environ['PATH'] = vipshome + ';' + os.environ['PATH']
 
 import pyvips
 
-image_path = rf'C:\Users\bunny\Desktop\VAN0005-RK-1-1-AF_preIMS_registered.ome1.ome.tiff'
+image_path = rf'X:\segmentation_mask_stitched.ome.tiff'
 # image_path = rf'G:\HuBMAP\Vanderbilt TMC\2020 Release Data\DR_reformat\VAN0005-RK-4-172\VAN0005-RK-4-172-AF\processedMicroscopy\VAN0005-RK-4-172-AF_preIMS_images\VAN0005-RK-4-172-AF_preIMS_registered.ome.tiff'
 
 whole_image = pyvips.Image.new_from_file(image_path, access='sequential')
@@ -26,7 +26,8 @@ for i in range(page_count):
     image = pyvips.Image.new_from_file(rf'temp/layer_{i}.tiff', access='sequential', )
     image = image.shrink(8, 8)
     # image = image.copy(xres=image.xres / 8, yres=image.yres / 8)
-    images.append(image)
+    # images.append(image)
+    image.write_to_file(rf'temp/layer_{i}_8.tiff')
 # for i in range(1, 4):
 #     temp = images[:]
 #     out = pyvips.Image.arrayjoin(temp, across=i)
@@ -35,9 +36,9 @@ for i in range(page_count):
 #     out.write_to_file(rf'C:\Users\bunny\Desktop\test_{i}.tiff')
 
 
-out = pyvips.Image.arrayjoin(images, across=1)
-out.set_type(pyvips.GValue.gint_type, "page-height", 3)
-print(out.get_fields())
-print([out.get(field) for field in out.get_fields()[:13]])
-print("writing tif ...")
-out.write_to_file(rf'C:\Users\bunny\Desktop\test.ome.tiff')
+# out = pyvips.Image.arrayjoin(images, across=1)
+# out.set_type(pyvips.GValue.gint_type, "page-height", 3)
+# print(out.get_fields())
+# print([out.get(field) for field in out.get_fields()[:13]])
+# print("writing tif ...")
+# out.write_to_file(rf'C:\Users\bunny\Desktop\test.ome.tiff')

2ab76efa732e977c984c9daa4700b4b6cc99f505
Yingnan Ju <bunny_gg@hotmail.com>
2020-11-17 15:53:20 -0500

    added full functional mask generator

--- hackathon/mask_generator.py
+++ hackathon/mask_generator.py
@@ -18,23 +18,57 @@ if len(sys.argv) >= 3:
 
 output_filename = json_filename.replace('json', 'tiff')
 
+shift_dict = {
+    # "VAN0003-LK-33-2": (1, 0, -6, 0, 1, -3),  # test
+    "VAN0009-LK-102-7": (1, 0, -6, 0, 1, -3),
+    "VAN0010-LK-155-40": (1, 0, -14, 0, 1, -5),
+    "VAN0016-LK-202-89": (1, 0, -3, 0, 1, -3),
+}
+
 read_file = open(json_filename, "r")
 data = json.load(read_file)
+
+agreement_dict = {}
+with open('agreement.csv', newline='') as inputfile:
+    for row in csv.reader(inputfile):
+        if row[0] in agreement_dict:
+            agreement_dict[row[0]].append(row[1])
+        else:
+            agreement_dict[row[0]] = []
 
 image_data = imread(image_filename)
 print("raw size: ", image_data.shape, "\tdata type: ", image_data.dtype)
 
 polys = []
+shift = np.asarray([0, 0])
+for key in shift_dict:
+    if key in json_filename:
+        shift[0] = shift_dict[key][2] * 8
+        shift[1] = shift_dict[key][5] * 8
+filter_list = []
+
+for key in agreement_dict:
+    if key in json_filename:
+        filter_list = agreement_dict[key]
+        print(filter_list)
+
+skip_count = 0
 for index in range(data.__len__()):
+    if str(index) in filter_list:
+        skip_count += 1
+        continue
     geom = np.array(data[index]['geometry']['coordinates'])
-    polys.append(geom)
+    shifted_geom = geom + shift
+    polys.append(shifted_geom)
+
+print(f'{skip_count} gloms skipped')
 
 # the shape of the image, h * w
 image_shape = []
 if len(image_data.shape) == 3:
     image_shape = image_data.shape[:2]
 if len(image_data.shape) == 5:
-    image_shape = image_data.shape[2:4]
+    image_shape = image_data.shape[3:]
 
 mask_1 = np.zeros(image_shape)
 for i in range(len(polys)):

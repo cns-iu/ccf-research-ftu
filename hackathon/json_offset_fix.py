a9d5df4b198301742b84d726d4c74bdc343311c2
Yingnan Ju <bunny_gg@hotmail.com>
2020-12-22 23:28:04 -0500

    added an condition to calculate index

--- hackathon/json_offset_fix.py
+++ hackathon/json_offset_fix.py
@@ -10,15 +10,18 @@ a_file = open(file_path, "r")
 json_object = json.load(a_file)
 a_file.close()
 
-offset = [0, 0]  # + right down  - left up
-index = 2
+offset = [-4, 10]  # + right down  - left up
+index = 1
 
 if len(sys.argv) >= 3:
     index = float(sys.argv[2])
 
 for row in json_object:
     old_list = row["geometry"]["coordinates"]
-    new_list = [[[(coor[0] + offset[0]) * index, (coor[1] + offset[1]) * index] for coor in old_list[0]]]
+    if index != 1:
+        new_list = [[[(coor[0] + offset[0]) * index, (coor[1] + offset[1]) * index] for coor in old_list[0]]]
+    else:
+        new_list = [[[coor[0] + offset[0], coor[1] + offset[1]] for coor in old_list[0]]]
     row["geometry"]["coordinates"] = new_list
 
 a_file = open(file_path, "w")
